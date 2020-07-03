abc8681ea7a58667a3ebed15b81a6604ee97bf57
Ted.H <50784573+cielgrisdemoscou@users.noreply.github.com>
2020-07-03 12:06:34 +0900

    #2020.07.03

--- todo/Controllers/TodoListViewController.swift
+++ todo/Controllers/TodoListViewController.swift
@@ -18,23 +18,8 @@ class TodoListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let newItem = Item()
-        newItem.title = "Find"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Find2"
-        itemArray.append(newItem2) 
-        
-        let newItem3 = Item()
-        newItem3.title = "Find3"
-        itemArray.append(newItem3)
-        
-        //        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-        //            itemArray = items
-        //        }
-        
+    
+        loadItems()
     }
     
     //MARK: - Tableview Datasource Methods
@@ -114,6 +99,17 @@ class TodoListViewController: UITableViewController {
         
         
         tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+            print("Error decoding item array, \(error)")
+            }
+        }
     }
     
 }
